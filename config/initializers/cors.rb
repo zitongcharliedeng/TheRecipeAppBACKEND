6025d1b8d0abf7514ddef40dc47abcf19fe74540
charlie <charlie@example.com>
2022-11-23 16:50:00 +0000

    typo fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://recipeappfrontend.netlify.app/'
+    origins 'https://recipeappfrontend.netlify.app'
     resource '*', headers: :any, methods: [:get, :post]
   end
 end
